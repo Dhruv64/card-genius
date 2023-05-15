4d0305f7e80d95bd4adc67f7696a04763b81922a
Dhruv Parmar <dhruv.p8601@gmail.com>
2023-05-15 18:33:26 +0530

    added save to contact feature

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,15 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import ToasterProvider from "~/providers/ToasterProvider";
 import { MantineProvider } from '@mantine/core';
 import { api } from "~/utils/api";
 
 //global icon import
-import { IconDefinition, library } from '@fortawesome/fontawesome-svg-core';
-import { faAt, faPhone, faMapPin, faLink } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedinIn, faWhatsapp, faFacebook, faYoutube, faTwitter, faGithub, faInstagram} from '@fortawesome/free-brands-svg-icons'
+// import { IconDefinition, library } from '@fortawesome/fontawesome-svg-core';
+// import { faAt, faPhone, faMapPin, faLink } from '@fortawesome/free-solid-svg-icons'
+// import { faLinkedinIn, faWhatsapp, faFacebook, faYoutube, faTwitter, faGithub, faInstagram} from '@fortawesome/free-brands-svg-icons'
 
-library.add(faAt, faPhone, faMapPin, faLink, faLinkedinIn, faWhatsapp, faFacebook, faYoutube, faTwitter, faGithub, faInstagram as IconDefinition)
-
+// library.add(faAt, faPhone, faMapPin, faLink, faLinkedinIn, faWhatsapp, faFacebook, faYoutube, faTwitter, faGithub, faInstagram as IconDefinition)
 
 import "~/styles/globals.css";
 
@@ -23,7 +21,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <MantineProvider withGlobalStyles withNormalizeCSS>
         <Component {...pageProps} />
-        <ToasterProvider />
       </MantineProvider>
     </SessionProvider>
   );
