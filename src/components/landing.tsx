376e2f9fca7364187eb580bcb36a80fa9cc47eea
Romil <patelromil.2001@gmail.com>
2023-05-15 11:20:08 +0530

    15/05/2023

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -8,6 +8,7 @@ import Detail1 from "./landingpage/detail1";
 import Detail2 from "./landingpage/detail2";
 import Detail4 from "./landingpage/detail4";
 import Detail3 from "./landingpage/detail3";
+import Carousel from "./landingpage/3dcarousel";
 <style>@import url(`https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap`);</style>
 
 
@@ -20,6 +21,7 @@ const LandingPage: NextPage = () => {
       <Detail1/>
       <Detail2/>
       <Detail3/>
+      <Carousel/>
       <Detail4/>      
       <Footer />
     </div>
